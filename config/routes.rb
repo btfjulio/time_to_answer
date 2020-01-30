db9d4e7de758c4d17a7b9cd6680e0e2395ad6bf5
Bruno Tostes <btfjulio@hotmail.com>
2020-01-30 07:55:10 -0300

    generate two devise scopes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
+  
   devise_for :admins
-
+  devise_for :users
+  
   get 'inicio', to: 'site/welcome#index'
   root to: 'welcome#index'
+
 end
